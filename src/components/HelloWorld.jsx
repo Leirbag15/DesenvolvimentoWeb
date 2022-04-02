5cb4a50e579fb5fcaf9257c8c40e25031236f298
Leirbag15 <gabriel.bezerra1501@gmail.com>
2022-04-02 11:29:17 -0300

    Enter

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 function HelloWorld() {
     return (
         <div>
-            <h1>Hello World React!</h1>
+            <h1>Hello World a React!</h1>
         </div>
     )
 }
