74c27d8b002201d8c53c7691d523c90e539b74fa
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2022-04-05 11:53:03 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+const App = () =>
+  <div className="App">
+    <World>
+ <Arena arena="Castelão">
+ <Hero name="Sicrano de Tal" >
+ <Enemy name="Fulano de Tal">
+ </Arena>
+ <Arena arena="Castelão">
+ <Hero name="Sicrano de Tal" >
+ <Enemy name="Fulano de Tal">
+ </Arena>
+ <Arena arena="Castelão">
+ <Hero name="Sicrano de Tal" >
+ <Enemy name="Fulano de Tal" >
+ </Arena>
+</World>
+  </div>
+  
+  export default App;
+
