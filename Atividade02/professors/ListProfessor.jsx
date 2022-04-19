97f9325338e411794540c0d03c3fce52e3aa0dba
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2022-04-19 10:26:59 -0300

    Create ListProfessor.jsx

---
+++ Atividade02/professors/ListProfessor.jsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+import ProfessorTableRow from "./ProfessorTableRow";
+import { professors } from '../data.js'
+
+function ListProfessor() {
+
+    /*const professors = [
+    {id:0, name: "John", university:"Johncity", degree:"Mestrado"},
+    {id:1, name: "Carlos", university:"UECE", degree:"Doutorado"},
+    {id:2, name: "Lucas", university:"UFC", degree:"Mestrado"},
+    {id:3, name: "Marcos", university:"Unifor", degree:"Doutorado"},
+]*/
+
+    function generateTable() {
+        if(!professors) return
+        return professors.map(
+            (professor,i) => {
+                return <ProfessorTableRow professor={professor} key={i} />
+            }
+        )
+    }
+
+    return (
+        <>
+            <main>
+                <h2>
+                    List Professor
+                </h2>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Universidade</th>
+                            <th>Titulação</th>
+                            <th colSpan={2} style={{textAlign:"center"}}>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {generateTable()}
+                    </tbody>
+                </table>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </>
+    );
+}
+
+export default ListProfessor
